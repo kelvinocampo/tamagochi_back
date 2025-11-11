7010af129173499385957c7d16d8147a8a9a0aa6
kelvinocampo <kevinocampooso@gmail.com>
2025-11-11 12:34:17 -0500

    refactor

--- src/service.ts
+++ src/service.ts
@@ -1,5 +1,5 @@
 import { Stats } from "./types";
-import { GoogleGenAI } from "@google/genai";
+import { GoogleGenAI, Part } from "@google/genai";
 import dotenv from "dotenv";
 
 dotenv.config()
@@ -23,19 +23,39 @@ export async function generalService(name: string, stats: Stats) {
 
 export async function IAGeneral(name: string, stats: Stats): Promise<[boolean, string]> {
     const status = true
-    const stringStats = JSON.stringify(stats)
-    const prompt = `
-        Eres una mascota dinosaurio amigable y tierno
-        Te llamas ${name}
-        Estas son tus estadisticas:
-        ${stringStats}
+    const statsDescription = `
+        **Estado actual de ${name}:**
+        - Hambre: ${stats.hunger}
+        - Felicidad: ${stats.happiness}
+        - Salud: ${stats.health}
+        - Energía: ${stats.energy}
+        - Vejiga: ${stats.bladder}
+        - Higiene: ${stats.hygiene}
+        - Durmiendo: ${stats.isSleeping ? 'Sí' : 'No'}
+    `;
+    const context: Part[] = [
+        {
+            text: `Responde en menos de 10 palabras según tu estado actual.
+           Las estadísticas van de 0% (peor) a 100% (mejor), 
+           excepto la vejiga: 0% es mejor (vacía) y 100% es peor (llena).
+           No incluyas emojis ni formato Markdown como \\n o similares.
+           No menciones valores numéricos en tu respuesta.`,
+        },
+        {
+            text: `Eres una mascota dinosaurio amigable y tierno. Tu nombre es ${name}. 
+               Comenta sobre tu estado o sobre alguna de tus estadísticas. 
+               Mantén el mensaje breve, corto y divertido.`,
+        },
+        {
+            text: statsDescription,
+        },
+    ];
+    console.log(context);
 
-        Que deseas comentar.
-    `
 
     const response = await ai.models.generateContent({
         model: "gemini-2.5-flash",
-        contents: prompt,
+        contents: context,
         config: {
             thinkingConfig: {
                 thinkingBudget: 0,
